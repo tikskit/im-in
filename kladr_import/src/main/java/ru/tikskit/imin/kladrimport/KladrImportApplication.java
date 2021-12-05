93eb317742a5ae6bacb6481ef7859d49fd65882c
tikskit <tikskit@gmail.com>
2021-12-05 13:25:24 +0700

    1. kladr_import module added; 2. .idea dir removed; 3. .gitignore fixed

---
+++ kladr_import/src/main/java/ru/tikskit/imin/kladrimport/KladrImportApplication.java
@@ -0,0 +1,13 @@
+package ru.tikskit.imin.kladrimport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KladrImportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KladrImportApplication.class, args);
+	}
+
+}
